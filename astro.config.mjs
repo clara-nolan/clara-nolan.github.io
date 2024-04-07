2eac20aca84f51ac409fdfd021cdf5d790018885
Clara <nolan.clara.us@gmail.com>
2024-04-06 22:38:59 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://clara-nolan.github.io',
-    base: 'clara-nolan.github.io',
   })
